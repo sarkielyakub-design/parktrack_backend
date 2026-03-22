3dfde8408abdf17628ffbe79f5425aae4102c80c
your name <you@email.com>
2026-03-22 23:47:14 +0100

    fix label generator path

--- app/utils/label_generator.py
+++ app/utils/label_generator.py
@@ -3,6 +3,9 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 
+BASE_DIR = os.getcwd()
+
+
 def generate_label(shipment, qr_path, barcode_path):
 
     os.makedirs("labels", exist_ok=True)
@@ -10,27 +13,46 @@ def generate_label(shipment, qr_path, barcode_path):
     filename = f"{shipment.tracking_id}.pdf"
 
     file_path = os.path.join(
+        BASE_DIR,
         "labels",
         filename
     )
 
-    # ✅ FIX PATHS
+    # ✅ FIX PATHS (important)
 
-    qr_real = qr_path.replace("/", "", 1) if qr_path.startswith("/") else qr_path
-    barcode_real = barcode_path.replace("/", "", 1) if barcode_path.startswith("/") else barcode_path
+    qr_real = os.path.join(
+        BASE_DIR,
+        qr_path.replace("/", "")
+    )
+
+    barcode_real = os.path.join(
+        BASE_DIR,
+        barcode_path.replace("/", "")
+    )
 
     c = canvas.Canvas(
         file_path,
         pagesize=letter
     )
 
-    # HEADER
+    # ================= HEADER =================
 
     c.setFillColorRGB(1, 1, 0)
-    c.rect(0, 700, 600, 80, fill=1)
+
+    c.rect(
+        0,
+        700,
+        600,
+        80,
+        fill=1
+    )
 
     c.setFillColorRGB(0, 0, 0)
-    c.setFont("Helvetica-Bold", 18)
+
+    c.setFont(
+        "Helvetica-Bold",
+        18
+    )
 
     c.drawString(
         50,
@@ -38,9 +60,12 @@ def generate_label(shipment, qr_path, barcode_path):
         "ZTECHTRACKIA EXPRESS"
     )
 
-    # TRACKING
+    # ================= TRACKING =================
 
-    c.setFont("Helvetica-Bold", 22)
+    c.setFont(
+        "Helvetica-Bold",
+        22
+    )
 
     c.drawString(
         50,
@@ -48,9 +73,12 @@ def generate_label(shipment, qr_path, barcode_path):
         shipment.tracking_id
     )
 
-    # INFO
+    # ================= INFO =================
 
-    c.setFont("Helvetica", 12)
+    c.setFont(
+        "Helvetica",
+        12
+    )
 
     c.drawString(50, 650, f"FROM: {shipment.from_city}")
     c.drawString(50, 630, f"TO: {shipment.to_city}")
@@ -58,7 +86,7 @@ def generate_label(shipment, qr_path, barcode_path):
     c.drawString(50, 590, f"Phone: {shipment.receiver_phone}")
     c.drawString(50, 570, f"Sender: {shipment.sender_name}")
 
-    # BARCODE
+    # ================= BARCODE =================
 
     if os.path.exists(barcode_real):
 
@@ -73,7 +101,7 @@ def generate_label(shipment, qr_path, barcode_path):
     else:
         print("BARCODE NOT FOUND:", barcode_real)
 
-    # QR
+    # ================= QR =================
 
     if os.path.exists(qr_real):
 
