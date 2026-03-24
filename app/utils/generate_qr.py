8a90a52c3f3d071539a48cd35f9869ba2a63eabe
your name <you@email.com>
2026-03-24 11:10:12 +0100

    label fix clean

--- app/utils/generate_qr.py
+++ app/utils/generate_qr.py
@@ -1,19 +1,33 @@
 import os
-import qrcode
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 BASE_DIR = os.getcwd()
 
+def generate_label(shipment, qr_path, barcode_path):
 
-def generate_qr(data, tracking):
-
-    folder = os.path.join(BASE_DIR, "qr_codes")
+    folder = os.path.join(BASE_DIR, "labels")
     os.makedirs(folder, exist_ok=True)
 
-    filename = f"{tracking}.png"
+    filename = f"{shipment.tracking_id}.pdf"
 
     path = os.path.join(folder, filename)
 
-    img = qrcode.make(data)
-    img.save(path)
+    print("LABEL SAVED:", path)
+
+    c = canvas.Canvas(path, pagesize=letter)
+
+    c.setFont("Helvetica-Bold", 16)
+    c.drawString(50, 750, "ZTECHTRACKIA EXPRESS")
+
+    c.drawString(50, 720, shipment.tracking_id)
+
+    if os.path.exists(barcode_path):
+        c.drawImage(barcode_path, 50, 600, 250, 80)
+
+    if os.path.exists(qr_path):
+        c.drawImage(qr_path, 320, 600, 120, 120)
+
+    c.save()
 
     return path
